eafb76872a1cebae3a06c5203a5289589fa89a2d
wenhuaiyu <wenhuaiyu@imobpay.com>
2016-11-22 11:19:46 +0800

    微信接口迁移-devbranch:1、常量类名修改

---
+++ src/main/java/com/imobpay/base/console/OuterConsoleColumn.java
@@ -0,0 +1,44 @@
+
+ /** 
+ * 包名: package UserConsoleColumn.console; <br/> 
+ * 添加时间: 2016年11月15日 下午6:38:57 <br/> 
+ */
+package com.imobpay.base.console;
+/**
+ * 
+ * <pre>
+ * 【类型】: OuterConsoleColum <br/> 
+ * 【作用】: 对外支付服务常量类. <br/>  
+ * 【时间】：2016年11月15日 下午6:39:22 <br/> 
+ * 【作者】：HuaiYu.Wen <br/> 
+ * </pre>
+ */
+public class OuterConsoleColumn {
+    /** 需要特殊处理的appuser和feeid(第一版只用于瑞钱包VIP)参数配置KEY */
+    public static final String VIPAPPUSER               = "VIPAPPUSER";
+    /** 机构号 */
+    public static final String BRANCHID                 = "branchId";
+    /** 微信用户openid */
+    public static final String OPENID_X                 = "openId";
+    /** 问候语 */
+    public static final String FIRSTVAL                 = "firstVal";
+    /** 备注 */
+    public static final String REMARKVAL                = "remarkVal";
+    /** 消息个数 */
+    public static final String WX_MSG_TEM_CONTENT_COUNT = "wxMsgTemContentCount";
+    /** 交易推送类型 */
+    public static final String WX_MSG_TRADE_TYPE        = "wxMsgTradeType";
+    /** 推送详情URL */
+    public static final String PUSH_URL                 = "pushUrl";
+    /** 推送内容 */
+    public static final String KEYWORDVAL               = "keyword{0}Val";
+    /** 用户ID */
+    public static final String TOUSER                   = "touser";
+    /** 模板ID */
+    public static final String TEMPLATE_ID              = "template_id";
+    /** 发送数据 */
+    public static final String DATA                     = "data";
+    /** 详情url */
+    public static final String URL                      = "url";
+}
+
